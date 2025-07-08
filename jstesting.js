cefbfeea03c9572dc06aff5348806418f1937431
Narek <n.a.77333@mail.ru>
2025-07-08 14:18:07 +0400

    Starting new project

---
+++ jstesting.js
@@ -0,0 +1,44 @@
+// Write the data to the supplied writable stream one million times.
+// Be attentive to back-pressure.
+/* function writeOneMillionTimes(writer, data, encoding, callback) {
+  let i = 1000000;
+  write();
+  function write() {
+    let ok = true;
+    do {
+      i--;
+      if (i === 0) {
+        // Last time!
+        writer.write(data, encoding, callback);
+      } else {
+        // See if we should continue, or wait.
+        // Don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      // Had to stop early!
+      // Write some more once it drains.
+      writer.once('drain', write);
+    }
+  }
+} 
+
+import crypto from 'crypto';
+
+const message = 'I have a secret and nobody knows it';
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let encrypted = cipher.update(message, 'utf8', 'hex');
+encrypted += cipher.final('hex');
+console.log(encrypted);
+
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+decrypted += decipher.final('utf8');
+console.log(decrypted);
+*/
+
